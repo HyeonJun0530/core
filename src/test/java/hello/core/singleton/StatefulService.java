436787e56ad29884c11f37ca9ec67ec4a0b9c810
hyeonjunkim <ksc6203@gmail.com>
2022-06-05 14:45:20 +0900

    스프링 기본

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,13 +2,14 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price; //상태를 유지하는 필드
-    public void order(String name, int price){
+//    private int price; //상태를 유지하는 필드 10000 -> 20000
+    public int order(String name, int price){
         System.out.println("name = " + name + " price = " + price);
-        this.price = price;
-    }
-
-    public int getPrice(){
+//        this.price = price; //여기가 문제
         return price;
     }
+
+//    public int getPrice(){
+////        return price;
+//    }
 }
